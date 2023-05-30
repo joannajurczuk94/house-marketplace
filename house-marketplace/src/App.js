e9c7b2b98fa698695b345d6788761e626250e74d
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-05-30 18:51:11 +0100

    navbar added

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -1,11 +1,12 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Explore from './pages/Explore'
-import Offers from "./pages/Offers"
-// import Profile from "./pages/Profile"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import ForgotPassword from "./pages/ForgotPassword"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+// import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
@@ -19,8 +20,8 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
+        <Navbar />
       </Router>
-      {/* Navbar */}
     </>
   );
 }
