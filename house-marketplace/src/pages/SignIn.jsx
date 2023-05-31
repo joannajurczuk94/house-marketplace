3d48c65c605177962e9dba2964d1e4dda4ad3b72
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-05-31 14:12:05 +0100

    SignUp page added

--- house-marketplace/src/pages/SignIn.jsx
+++ house-marketplace/src/pages/SignIn.jsx
@@ -12,7 +12,12 @@ function SignIn() {
   const { email, password } = formData;
   const navigate = useNavigate();
 
-  const onChange = () => {};
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
 
   return (
     <>
@@ -21,48 +26,51 @@ function SignIn() {
           <p className="pageHeader">Welcome Back!</p>
         </header>
 
-          <form>
+        <form>
+          <input
+            type="email"
+            className="emailInput"
+            placeholder="Email"
+            id="email"
+            value={email}
+            onChange={onChange}
+          />
+
+          <div className="passwordInputDiv">
             <input
-              type="email"
-              className="emailInput"
-              placeholder="Email"
-              id="email"
-              value={email}
+              type={showPassword ? "text" : "password"}
+              className="passwordInput"
+              placeholder="Password"
+              id="password"
+              value={password}
               onChange={onChange}
             />
 
-            <div className="passwordInputDiv">
-              <input
-                type={showPassword ? "text" : "password"}
-                className="passwordInput"
-                placeholder="Password"
-                id="password"
-                value={password}
-                onChange={onChange}
-              />
+            <img
+              src={visibilityIcon}
+              alt="show password"
+              className="showPassword"
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+          </div>
 
-              <img
-                src={visibilityIcon}
-                alt="show password"
-                className="showPassword"
-                onClick={() => setShowPassword((prevState) => !prevState)}
-              />
-            </div>
+          <Link to="/forgot-password" className="forgotPassword">
+            Forgot Password
+          </Link>
 
-            <Link to='/forgot-password' className="forgotPassword">Forgot Password</Link>
-         
-         <div className="signInBar">
-          <p className="signInText">
-            Sign In
-          </p>
-          <button className="signInButton">
-            <ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
-          </button>
-         </div>
+          <div className="signInBar">
+            <p className="signInText">Sign In</p>
+            <button className="signInButton">
+              <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+            </button>
+          </div>
+        </form>
 
-         
-          </form>
-       
+        {/* Google OAuth */}
+
+        <Link to="/sign-up" className="registerLink">
+          Sign Up Instead
+        </Link>
       </div>
     </>
   );
