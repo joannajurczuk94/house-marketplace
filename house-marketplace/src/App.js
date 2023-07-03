ae7481a50e8413f3891b48cd659ef5b53a802911
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-07-03 17:24:13 +0100

    React-Toastify installed;  bad request added

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {ToastContainer} from 'react-toastify';
+ import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./components/Navbar";
-
 import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
 // import Profile from "./pages/Profile"
@@ -20,9 +21,12 @@ function App() {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+       
         </Routes>
         <Navbar />
       </Router>
+
+      <ToastContainer />
     </>
   );
 }
