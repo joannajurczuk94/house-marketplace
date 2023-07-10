4dc87aa8c75198977bdb9580fede9e97817c275d
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-07-10 16:31:45 +0100

    'Could not update profile details' error added

--- house-marketplace/src/pages/Profile.jsx
+++ house-marketplace/src/pages/Profile.jsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, userRef } from "react";
 import { getAuth, updateProfile } from "firebase/auth";
-import { updateDoc } from "firebase/firestore";
+import { updateDoc, doc } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { useNavigate, Link } from "react-router-dom";
+import {toast} from 'react-toastify';
 
 function Profile() {
   const auth = getAuth();
@@ -21,8 +22,23 @@ function Profile() {
     navigate("/");
   };
 
-  const onSubmit = () => {
-    console.log(123);
+  const onSubmit = async () => {
+    try {
+      if(auth.currentUser.displayName !== name) {
+        // Update display name in fb
+        await updateProfile(auth.currentUser, {
+          displayName: name
+        })
+
+        //Update in firestore
+        const userRef = doc(db, 'users'.auth.currentUser.uid)
+        await updateDoc(userRef, {
+          name
+        })
+      }
+    } catch (error) {
+toast.error('Could not update profile details')
+    }
   };
 
 const onChange = (e) => {
