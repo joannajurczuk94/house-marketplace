f063f841a85c637f46228f5a07be02ee52a404b5
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-07-18 17:41:46 +0100

    component PrivateRoute added

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {ToastContainer} from 'react-toastify';
- import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
 import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
 // import Profile from "./pages/Profile"
@@ -17,11 +18,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-       
         </Routes>
         <Navbar />
       </Router>
