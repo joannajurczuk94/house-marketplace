b0f3cac827b7c8828b90223f31a2322ef3b85161
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-07-21 16:38:38 +0100

    ForgotPassword Added

--- house-marketplace/src/pages/ForgotPassword.jsx
+++ house-marketplace/src/pages/ForgotPassword.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { toast } from "react-toastify";
+import { reactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+
 function ForgotPassword() {
+  const [email, setEmail] = useState("");
+
+  const onChange = (e) => {};
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Forgot Password</h1>
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Forgot Password</p>
+      </header>
+
+      <main>
+        <form onSubmit={onSubmit}>
+          <input
+            type="email"
+            className="emailInput"
+            placeholder="Email"
+            id="email"
+            value={email}
+            onChange={onChange}
+          />
+          <Link className="forgotPasswordLink" to="/sign-in">
+            Sign In
+          </Link>
+        </form>
+      </main>
     </div>
   );
 }
