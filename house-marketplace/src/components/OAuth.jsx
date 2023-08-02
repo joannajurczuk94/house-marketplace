2d5baebf88d5335a0cda1ec7dcf83056f4d47866
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-08-02 17:44:33 +0100

    Update OAuth.jsx (jmports)

--- house-marketplace/src/components/OAuth.jsx
+++ house-marketplace/src/components/OAuth.jsx
@@ -1,5 +1,9 @@
 import {useLocation, useNavigate} from "react-router-dom"
 import {getAuth, signInWithPopup, GoogleAuthProvider} from "firebase/auth"
+import {doc, setDoc, getDoc} from "firebase/firestore";
+import {db} from "../firebase.config";
+import {toast} from "react-toastify";
+import googleIcon from "../assets/svg/googleIcon.svg"
 
 function OAuth() {
   return <div>oauth</div>;
