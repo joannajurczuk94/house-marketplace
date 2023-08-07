b6d7641430da78e06f5503f2ac60c363a52e2ced
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-08-07 17:41:34 +0100

    Check for user

--- house-marketplace/src/components/OAuth.jsx
+++ house-marketplace/src/components/OAuth.jsx
@@ -9,13 +9,32 @@ function OAuth() {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const onGoogleClick = () => {}
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user
+
+      //Check for user
+      const docRef = doc(db, "users", user.uid)
+      const docSnap = await getDoc(docRef)
+
+      //If user doesn't exist, create user
+      if(!docSnap.exists()) {
+        await setDoc(doc(db, "users", user.uid), {
+          
+        })
+
+      }
+    } catch (error) {}
+  };
 
   return (
     <div className="socialLogin">
       <p>Sign {location.pathname === "/sign-up" ? "up" : "in"}</p>
       <button className="socialIconDiv" onClick={onGoogleClick}>
-        <img className = "socialIconImg" src={googleIcon} alt="google" />
+        <img className="socialIconImg" src={googleIcon} alt="google" />
       </button>
     </div>
   );
