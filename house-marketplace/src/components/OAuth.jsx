8aaf29ae78ef37a11932e2f00c1abd247a535cde
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-08-10 15:42:19 +0100

    serverTimestamp added

--- house-marketplace/src/components/OAuth.jsx
+++ house-marketplace/src/components/OAuth.jsx
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import { doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { toast } from "react-toastify";
 import googleIcon from "../assets/svg/googleIcon.svg";
@@ -25,7 +25,7 @@ function OAuth() {
         await setDoc(doc(db, "users", user.uid), {
           name: user.displayName,
           email: user.email,
-          
+          timestamp: serverTimestamp()
         })
 
       }
