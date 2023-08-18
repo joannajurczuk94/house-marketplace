968cd8869f0f0706ecaa387871d785f52ef476e3
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-08-18 16:28:06 +0100

    imports added to Category.jsx

--- house-marketplace/src/pages/Category.jsx
+++ house-marketplace/src/pages/Category.jsx
@@ -1,3 +1,10 @@
+import { useEffect, useState } from "react"
+import {useParams} from "react-router-dom";
+import {collection, getDocs, query, where, orderBy, limit, startAfter} from "firebase/firestore";
+import {db} from "../firebase.config";
+import {toast} from "react-toastify"
+import Spinner from "../components/Spinner"
+
 function Category() {
     return (
         <div>
