d164e7ae44830170406a7ac6f03ea40a19b2a4b5
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-08-18 16:37:16 +0100

    Update Category.jsx

--- house-marketplace/src/pages/Category.jsx
+++ house-marketplace/src/pages/Category.jsx
@@ -1,16 +1,52 @@
-import { useEffect, useState } from "react"
-import {useParams} from "react-router-dom";
-import {collection, getDocs, query, where, orderBy, limit, startAfter} from "firebase/firestore";
-import {db} from "../firebase.config";
-import {toast} from "react-toastify"
-import Spinner from "../components/Spinner"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
 
 function Category() {
-    return (
-        <div>
-category
-        </div>
-    )
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        //Get reference
+        const listingsRef = collection(db, "listings");
+
+        //Create a query
+        const q = query(
+          listingsRef,
+          where("type", "==", params.categoryName),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
+
+        // Execute query
+        const querySnap = await getDocs(q);
+
+        let listings = [];
+
+        querySnap.forEach((doc) => {
+          console.log(doc.data());
+        });
+      } catch (error) {}
+    };
+    fetchListings();
+  });
+
+  return <div>category</div>;
 }
 
-export default Category
+export default Category;
