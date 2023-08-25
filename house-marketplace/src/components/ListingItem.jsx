54816166b4b1c3dbad7d6e1f0722b41383b4ae5f
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-08-25 10:37:27 +0100

    Number of bathrooms added

--- house-marketplace/src/components/ListingItem.jsx
+++ house-marketplace/src/components/ListingItem.jsx
@@ -29,12 +29,20 @@ function ListingItem({ listing, id }) {
                 : listing.regularPrice
                     .toString()
                     .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                    {listing.type === "rent" && "/Month"}
+              {listing.type === "rent" && "/Month"}
             </p>
             <div className="categoryListingInfoDiv">
               <img src={bedIcon} alt="bed" />
               <p className="categoryListingInfoText">
-                {listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : '1 Bedroom'}
+                {listing.bedrooms > 1
+                  ? `${listing.bedrooms} Bedrooms`
+                  : "1 Bedroom"}
+              </p>
+              <img src={bathtubIcon} alt="bath" />
+              <p className="categoryListingInfoText">
+                {listing.bathrooms > 1
+                  ? `${listing.bathrooms} Bathrooms`
+                  : "1 Bathroom"}
               </p>
             </div>
           </div>
