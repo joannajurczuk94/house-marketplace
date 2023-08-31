ade12627be91b1dbe3e2e8153d517be48c2cb13a
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-08-31 13:50:12 +0100

    DeleteIcon added

--- house-marketplace/src/components/ListingItem.jsx
+++ house-marketplace/src/components/ListingItem.jsx
@@ -3,7 +3,7 @@ import { ReactComponent as DeleteIcon } from "../assets/svg/deleteIcon.svg";
 import bedIcon from "../assets/svg/bedIcon.svg";
 import bathtubIcon from "../assets/svg/bathtubIcon.svg";
 
-function ListingItem({ listing, id }) {
+function ListingItem({ listing, id, onDelete }) {
   return (
     <div>
       <li className="categoryListing">
@@ -47,6 +47,14 @@ function ListingItem({ listing, id }) {
             </div>
           </div>
         </Link>
+
+        {onDelete && (
+          <DeleteIcon
+            className="removeIcon"
+            fill="rgb(231,76,60)"
+            onClick={() => onDelete(listing.id, listing.name)}
+          />
+        )}
       </li>
     </div>
   );
